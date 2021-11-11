ea5f0c56de3abe4c8b927218332c60abccd203be
ZhouKanglei <47837939+ZhouKanglei@users.noreply.github.com>
2021-11-11 11:38:13 +0800

    update

--- gpu_test.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-print(tf.__version__)
-print(tf.test.is_built_with_cuda())
-print(tf.test.is_gpu_available())
--- grid_search.py
+++
@@ -1,8 +0,0 @@
-from exp_main import *
-from data.NYU.corrupt import *
-
-# main()
-
-for pos in range(25):
-    train_model = train(start_pos=1)
-    test(model_path =train_model, search_opt=True)
